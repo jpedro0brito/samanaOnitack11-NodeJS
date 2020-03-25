328bd9be9c18334b5d8d847fe1af63deb02f25ae
JPedro Brito <jpedro0brito@gmail.com>
2020-03-25 19:04:07 -0400

    criando o backend da aplicacao

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const app = express();
-
-app.get('/', (res, quest) => {
-
-});
-
-app.listen(3333);
---
+++ src/routes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const OngControllers = require('./controllers/OngsControllers');
+const IncidentController = require('./controllers/IncidentController');
+const SessionController = require('./controllers/SessionController');
+
+const routes = express.Router();
+
+routes.post('/sessions', SessionController.create);
+
+routes.get('/ongs', OngControllers.index);
+routes.post('/ongs', OngControllers.create);
+
+routes.get('/profile', ProfileController.index);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+module.exports = routes;
